ef12b2bbc802ddafcd5a1e21c6eee0d5b18efe22
KaLuLas <kalulas41@gmail.com>
2018-11-22 00:36:21 +0800

    Collision Update
    +修改了地形碰撞区
    +新增了受击动画（透明度
    +玩家现在被击播放动画是无敌的

---
+++ Assets/SortLayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class SortLayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		int layer = 0;
+		Dictionary<GameObject,float> SortDict = new Dictionary<GameObject,float>();
+		GameObject[] SortList = new GameObject[50];
+		SortList = GameObject.FindGameObjectsWithTag("NeedSort");
+		foreach( GameObject Object in SortList){
+			//Debug.Log(Object);
+			float y = Object.GetComponent<Transform>().position.y;
+			SortDict.Add(Object, y);
+		}
+		var SortedDict = from entry in SortDict orderby entry.Value descending select entry;
+		Debug.Log(SortedDict);
+		foreach( var Object in SortedDict){
+			//if(Object.Key.GetComponent<SpriteRenderer>())
+			Object.Key.GetComponent<Renderer>().sortingOrder = layer;
+			//else
+			//Object.Key.GetComponent<Renderer>().sortingOrder = layer;
+			layer ++;
+		}
+	}
+}
