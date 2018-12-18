ff6b817d8f7edaa8ea73d4850915b13867368eac
KaLuLas <kalulas41@gmail.com>
2018-12-19 00:31:06 +0800

    完工 UI & BGM 完成
    项目暂时告一段落啦！
    接下来就是加加细节或者调调BUG之类的

---
+++ Assets/HealthUpdate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class HealthUpdate : MonoBehaviour {
+	public GameObject Player;
+	public GameObject Boss;
+	private float PlayerMaxHealth;
+	private float PlayerCurrentHealth;
+	public SimpleHealthBar PlayerHealthBar;
+	private float BossMaxHealth;
+	private float BossCurrentHealth;
+	public SimpleHealthBar BossHealthBar;
+
+	// Use this for initialization
+	void Start () {
+		PlayerMaxHealth = (float)Variables.Application.Get("PlayerHealthMax");
+		if(Boss) BossMaxHealth = (float)Variables.Object(Boss).Get("Health");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerCurrentHealth = (float)Variables.Object(Player).Get("Health");
+		PlayerHealthBar.UpdateBar( PlayerCurrentHealth, PlayerMaxHealth );
+		if(Boss){
+			BossCurrentHealth = (float)Variables.Object(Boss).Get("Health");
+			BossHealthBar.UpdateBar( BossCurrentHealth, BossMaxHealth );
+		}
+	}
+}
